065007f872a8e113b96836893daeaa76580745e4
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2022-09-03 00:41:42 +0300

    added base methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
@@ -5,3 +9,25 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        self.go_to_element(self.element_is_present(locator))
+        return wait(self.browser, timeout).until(EC.visibility_of_element_located(locator))
+
+    def elements_are_visible(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.presence_of_element_located(locator))
+
+    def elements_are_present(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def element_is_not_visible(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.invisibility_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):
+        self.browser.execute_script("arguments[0].scrollIntoView();", element)
